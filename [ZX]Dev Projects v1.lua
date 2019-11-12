1f566fb6edbe9cfe75095d4b9ea53ad7dfa064af
ZxTeamsProjects <57673051+ZxTeamsProjects@users.noreply.github.com>
2019-11-12 22:20:59 +0800

    Test
    Test Execution

---
+++ [ZX]Dev Projects v1.lua
@@ -0,0 +1,435 @@
+loadfile = function() end
+local GG_Name = "com.jzgt" -- change to your gg package!
+local GG_V = gg.PACKAGE
+local GG_A = GG_V:find(GG_Name)
+if GG_A == 1 then
+else
+gg.alert("❎ Error! Please use GG Mod JZ!", "Copy Download Link") gg.copyText("https://t.me/Jzmenu/7") 
+You_Got_Error_There_No_Anti_Os.exit_And_No_Function()
+end
+if gg.isPackageInstalled("com.gxlkj.tl") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Logger Package : 'com.gxlkj.tl' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("com.decrypt.revo6") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Logger Package : 'com.decrypt.revo6' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("catch_.me_.if_.you_.can") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Forbidden Package : 'catch_.me_.if_.you_.can' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("sstool.only.com.sstool") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Logger Package : 'sstool.only.com.sstool' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("serdadu.log.revo6") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Logger Package : 'serdadu.log.revo6' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("com.modeghaith.hd") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Logger Package : 'com.modeghaith.hd' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("com.wtkc.gg") then
+  gg.alert("⛔Error ZSecurity\n SLua Detected Logger Package : 'com.wtkc.gg' Make sure to Delete this package to use this script")
+  os.exit()
+else
+end
+if gg.isPackageInstalled("io.neoterm") then
+  gg.alert("⚠️Warning ZSecurity\n SLua Detected CoZXand Package : 'io.neoterm' This package do multipes coZXand and can decrypt script ")
+  os.remove(gg.EXT_STORAGE .. "/Unluac")
+  os.remove(gg.EXT_STORAGE .. "/Unluac.jar")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac.jar")
+  os.remove(gg.EXT_STORAGE .. "/Unluac_2019")
+  os.remove(gg.EXT_STORAGE .. "/Unluac_2019.jar")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac_2019")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac_2019.jar")
+else
+end
+if gg.isPackageInstalled("com.termux") then
+  gg.alert("⚠️Warning ZSecurity\n SLua Detected CoZXand Package : 'com.termux' This package do multipes coZXand and can decrypt script ")
+  os.remove(gg.EXT_STORAGE .. "/Unluac")
+  os.remove(gg.EXT_STORAGE .. "/Unluac.jar")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac.jar")
+  os.remove(gg.EXT_STORAGE .. "/Unluac_2019")
+  os.remove(gg.EXT_STORAGE .. "/Unluac_2019.jar")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac_2019")
+  os.remove(gg.EXT_STORAGE .. "/Download/Unluac_2019.jar")
+else
+end
+local function loader(str)
+local i = "";repeat i = i.. 
+string.char(math.random(97,122)) 
+until #i > 10;package.path = "?";local ii = (gg.EXT_STORAGE).."/"..i;io.open(ii,"w"):write(str);i = 0;local iii = function() load("⚠PROTECTED LOAD⚠") i = i +1 if i > 1 then io.open(ii,"w"):write(str) os.remove(ii) debug.sethook(iii,"") end end;debug.sethook(iii,"cr");local iiii = pcall(require,ii) return end
+
+
+ct = "ᶻˣJᴇᴛZ"
+
+os.remove(gg.EXT_STORAGE .. "/Decrypt.lua")
+os.remove(gg.EXT_STORAGE .. "/Revo.lua")
+os.remove(gg.EXT_STORAGE .. "/мм²⁵¹²༒Hσσк Dєтє¢тє")
+os.remove(gg.EXT_STORAGE .. "/мм²⁵¹²༒Hσσк Dєтє¢тє.lua")
+os.remove(gg.EXT_STORAGE .. "/ZX2512")
+os.remove(gg.EXT_STORAGE .. "/ZX2512.lua")
+os.remove(gg.EXT_STORAGE .. "/мм²⁵¹²༒.lua")
+os.remove(gg.EXT_STORAGE .. "/мм²⁵¹²༒")
+os.remove(gg.EXT_STORAGE .. "/Mayimi")
+os.remove(gg.EXT_STORAGE .. "/Ƭ𝔦ន ղ𝖖մƴҽɳ")
+os.remove(gg.EXT_STORAGE .. "/Ƭ𝔦ន ղ𝖖մƴҽɳ.lua")
+os.remove(gg.EXT_STORAGE .. "/ᴍᴜʜᴀᴍᴀᴅɢᴀᴍᴇʀ.ʟᴜᴀ")
+os.remove(gg.EXT_STORAGE .. "/Dec By Mr Joker.Lua")
+os.remove(gg.EXT_STORAGE .. "/MrVinYT")
+os.remove(gg.EXT_STORAGE .. "/[SSDECOMP]1.lua")
+os.remove(gg.EXT_STORAGE .. "/[SSDECOMP].lua")
+os.remove(gg.EXT_STORAGE .. "/SSDECOMP1LE")
+os.remove(gg.EXT_STORAGE .. "/[SSDECOMP]ZSecurity.ZX.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/Decrypt.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/Revo.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/мм²⁵¹²༒Hσσк Dєтє¢тє")
+os.remove(gg.EXT_STORAGE .. "/Download/мм²⁵¹²༒Hσσк Dєтє¢тє.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/ZX2512")
+os.remove(gg.EXT_STORAGE .. "/Download/ZX2512.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/мм²⁵¹²༒.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/мм²⁵¹²༒")
+os.remove(gg.EXT_STORAGE .. "/Download/Mayimi")
+os.remove(gg.EXT_STORAGE .. "/Download/Ƭ𝔦ន ղ𝖖մƴҽɳ")
+os.remove(gg.EXT_STORAGE .. "/Download/Ƭ𝔦ន ղ𝖖մƴҽɳ.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/ᴍᴜʜᴀᴍᴀᴅɢᴀᴍᴇʀ.ʟᴜᴀ")
+os.remove(gg.EXT_STORAGE .. "/Download/Dec By Mr Joker.Lua")
+os.remove(gg.EXT_STORAGE .. "/Download/MrVinYT")
+os.remove(gg.EXT_STORAGE .. "/Download/[SSDECOMP]1.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/[SSDECOMP].lua")
+os.remove(gg.EXT_STORAGE .. "/Download/SSDECOMP1LE")
+os.remove(gg.EXT_STORAGE .. "/Download/[SSDECOMP]ZSecurity.ZX.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/SSDECOMP1LE")
+os.remove(gg.EXT_STORAGE .. "/Compiler.lua")
+os.remove(gg.EXT_STORAGE .. "/Download/Compiler.lua")
+os.remove(gg.EXT_STORAGE .. "/Tencent")
+os.remove(gg.EXT_STORAGE .. "/SSDECOMP")
+os.remove(gg.EXT_STORAGE .. "/SSDECOMP1LE")
+os.remove(gg.EXT_STORAGE .. "/MrVinYT")
+os.remove(gg.EXT_STORAGE .. "/Mayimi")
+os.remove(gg.EXT_STORAGE .. "/Decrypt")
+os.remove(gg.EXT_STORAGE .. "/Revo Logger")
+os.remove(gg.EXT_STORAGE .. "/Revo 6 Logger")
+os.remove(gg.EXT_STORAGE .. "/QQ")
+os.remove(gg.EXT_STORAGE .. "/tencent")
+gg.alert(os.date("♾Ｙｏｕｒ Ｉｎｆｏ♾\n\n⎚ Procces : 『" .. gg.getTargetInfo().name .. "』\n⎚ Package Name : 『" .. gg.getTargetInfo().processName .. "』\n⎚ Game Version : 『" .. gg.getTargetInfo().versionName .. "』\n⎚ GameGuardian Version : 『" .. gg.VERSION .. "』\n⎚ Creator : 『"..ct.."』\n\n♾Time : %X \n♾DATE : %A, %d %B %Y. "))
+off = "〖🄾🄽〗"
+on = "〖🄾🄵🄵〗"
+test = on
+procs = "ɴᴏᴛ ʀᴜɴɴɪɴɢ"
+function Main()
+pack = gg.getTargetPackage()
+if pack == "com.rtsoft.growtopia" then
+procs = "ʀᴜɴɴɪɴɢ"
+end
+zx = gg
+local Menu = gg.choice({
+ "⎚ sᴛᴀᴛᴜs ᴅᴇᴠ\n╰" ..test,
+ "⎚ ᴍᴇɴᴜ\n╰〖🅉🅇〗",
+ "⎚ ᴏᴛʜᴇʀs\n╰〖🅉🅇〗",
+ "❌EXIT❌",
+},nil,"👊ɢʀᴏᴡᴛᴏᴘɪᴀ sᴛᴀᴛᴜs : "..procs.."\n🕷️sᴄʀɪᴘᴛ ʙʏ ZxTᴇᴀᴍ\n▶️sᴜᴘᴘᴏʀᴛ ᴄʀᴇᴀᴛᴏʀ ᴡɪᴛʜ sᴜʙsᴄʀɪʙᴇ")
+if Menu == nil then else
+if Menu == 1 then
+ if test == on then
+  test1(on)
+  test = off
+ else
+  test2(off)
+  test = on
+ end
+ end
+if Menu == 2 then Dark() end
+if Menu == 3 then Oth() end
+if Menu == 4 then exit() end
+end
+Jz = -1
+end
+Gt = 1
+
+function test1(on)
+if gg.getTargetInfo().packageName ~= "com.rtsoft.growtopia" then
+  gg.alert("🇮🇩Indonesia\nPilih Proses Growtopia\n\n🇬🇧English\nSelect Procces Growtopia")
+  os.exit()
+end
+gg.getListItems(1710)
+gg.getResults(1710)
+gg.editAll("1", gg.TYPE_DWORD)
+gg.toast("💻Dev On")
+end
+
+function test2(off)
+gg.getListItems(1710)
+gg.getResults(1710)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("🖥️Dev Off")
+end
+
+function Dark()
+prot = "Gʀᴏᴡᴛᴏᴘɪᴀ Nᴏᴛ Rᴜɴɴɪɴɢ Yᴇᴛ"
+packs = gg.getTargetPackage()
+if packs == "com.rtsoft.growtopia" then
+prot = "ʀᴜɴɴɪɴɢ"
+end
+local devm = gg.choice({
+	"⎚ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴠ1",
+	"⎚ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴠ2",
+	"⎚ ᴅᴇᴠᴇʟᴏᴘᴇʀ v3",
+	"⎚ ᴅᴇᴠᴇʟᴏᴘᴇʀ v4",
+},nil,"ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴍᴇɴᴜ ɢʀᴏᴡᴛᴏᴘɪᴀ\nSᴛᴀᴛᴜs: "..prot.."")
+if devm == nil then
+else
+if devm == 1 then
+if gg.getTargetInfo().packageName ~= "com.rtsoft.growtopia" then
+  gg.alert("🇮🇩Indonesia\nPilih Proses Growtopia\n\n🇬🇧English\nSelect Procces Growtopia")
+  os.exit()
+end
+dfast1 = gg.alert("➮Which The Version", "V1", "V2","V3")
+if dfast1 == nil then
+ else
+if dfast1 == 1 then
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.alert("➮Select Wrench Bro(This is V1)", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;32W;1D::145", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.alert("➮Select Fist Bro", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;18W;1D::145", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+t = gg.getResults(1710)
+gg.addListItems(t)
+gg.toast("➮Done")
+end
+if dfast1 == 2 then
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.alert("➮Select Wrench Bro(This is V2)", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;32D;1D::145", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.alert("➮Select Fist Bro", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;18D;1D::145", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+t = gg.getResults(1710)
+gg.addListItems(t)
+gg.toast("➮Done")
+end
+if dfast1 == 3 then
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.alert("➮Select Fist Bro\n(This is V3)", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;18W;1D", gg.TYPE_DWORD)
+gg.alert("➮Select Wrench Bro", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;32W;1D::145", gg.TYPE_DWORD)
+t = gg.getResults(1710)
+gg.addListItems(t)
+gg.toast("➮Done")
+end
+end
+Jz = -1
+end
+if devm == 2 then
+if gg.getTargetInfo().packageName ~= "com.rtsoft.growtopia" then
+  gg.alert("🇮🇩Indonesia\nPilih Proses Growtopia\n\n🇬🇧English\nSelect Procces Growtopia")
+  os.exit()
+end
+dfast = gg.alert("➮Which The Version", "V1", "V2","V3")
+if dfast == nil then
+ else
+if dfast == 1 then
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.alert("➮Select Fist Bro(This is V1)", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;18W", gg.TYPE_DWORD)
+gg.alert("➮Select Wrench Bro", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;32W::145", gg.TYPE_DWORD)
+t = gg.getResults(1710)
+gg.addListItems(t)
+gg.toast("➮Done")
+end
+if dfast == 2 then
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.alert("➮Select Fist Bro(This is V2)", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;18D", gg.TYPE_DWORD)
+gg.alert("➮Select Wrench Bro", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;32D::145", gg.TYPE_DWORD)
+t = gg.getResults(1710)
+gg.addListItems(t)
+ huyi = gg.toast("➮Done")
+end
+if dfast == 3 then
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.alert("➮Select Fist Bro\n(This is V3)", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;18W;1D", gg.TYPE_DWORD)
+gg.alert("➮Select Wrench Bro", "5 Sec")
+gg.sleep(5000)
+gg.searchNumber("0D;32W;1D::145", gg.TYPE_DWORD)
+t = gg.getResults(1710)
+gg.addListItems(t)
+gg.toast("➮Done")
+end
+end
+Jz = -1
+end
+if devm == 3 then
+if gg.getTargetInfo().packageName ~= "com.rtsoft.growtopia" then
+  gg.alert("🇮🇩Indonesia\nPilih Proses Growtopia\n\n🇬🇧English\nSelect Procces Growtopia")
+  os.exit()
+end
+dv3()
+end
+Jz = -1
+end
+if devm == 4 then
+if gg.getTargetInfo().packageName ~= "com.rtsoft.growtopia" then
+  gg.alert("🇮🇩Indonesia\nPilih Proses Growtopia\n\n🇬🇧English\nSelect Procces Growtopia")
+  os.exit()
+end
+dv4()
+  end
+  Jz = -1
+  end
+  
+  function dv3()
+gg.clearResults()
+  gg.alert("Select Wrench! You Have","3Sec")
+  gg.sleep(3000)
+  gg.searchNumber("0D;32W;1D:", gg.TYPE_DWORD)
+  gg.alert("Select Growpedia! You Have","3Sec")
+  gg.sleep(3000)
+  gg.refineNumber("0D;6336W;1D::145", gg.TYPE_DWORD)
+  gg.setVisible(false)
+  gg.setVisible(false)
+  gg.toast("Succed")
+end
+
+
+function dv4()
+gg.clearResults()
+            gg.alert("please, select wrench", "5 sec")
+            gg.sleep(5000)
+            gg.searchNumber("0D;32D;1D::145", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+            gg.alert("please, select fist", "5 sec")
+            gg.sleep(5000)
+            gg.searchNumber("0D;18D;1D::145", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+            t = gg.getResults(1710)
+            gg.addListItems(t)
+            gg.toast("Succes")
+  end
+
+
+
+function Oth()
+local Oths = gg.choice({
+	"⎚ ᴄᴀʟᴄᴜʟᴀᴛᴏʀ",
+	"⎚ ᴄʜᴀɴɢᴇʀ ɪᴛᴇᴍ",
+	"⎚ ᴄʟᴇᴀʀ ʀᴇsᴜʟᴛs",
+	"⎚ ᴀʙᴏᴜᴛ",
+})
+if Oths == nil then
+else
+if Oths == 1 then
+calcu()
+end
+if Oths == 2 then
+ci()
+end
+if Oths == 3 then
+clt()
+end
+if Oths == 4 then
+abt()
+end
+end
+Jz = -1
+end
+
+function calcu()
+gg.alert("Calculator Help Full For Verification GrowTopia")
+  t = gg.prompt({
+    "Value 1 :",
+    "Value 2 :"
+  }, {
+    [1] = 0,
+    [2] = 0
+  }, {
+    [1] = "number",
+    [2] = "number"
+  })
+  Jz = -1
+gg.alert(t[1] .. " x " .. t[2] .. " = "..(t[1] * t[2]))
+gg.alert(t[1] .. " + " .. t[2] .. " = "..(t[1] + t[2]))
+end
+
+function ci()
+gg.alert('Anti Gravity: 4992\nPjammer: 1276')
+xp = gg.prompt({
+"Input The ID : "
+}, nil, {"number"})
+if xp ~= nil and tonumber(xp[1]) then
+gg.getResults(1710)
+gg.editAll(xp[1], gg.TYPE_WORD)
+gg.toast(" Activated ")
+end
+Jz = -1
+end
+
+function clt()
+gg.clearList()
+gg.clearResults()
+gg.toast("⚠️Done Clear Result ⚠️\nAnd Dont Forget To Subscribe ↪️JetZ GT↩️\n🛡️Thx To :\n➡️Kocak Z\n➡️Rhack\n➡️Baldi Gamers\n➡️Gucktube YT")
+gg.sleep(600)
+end
+
+
+function abt()
+gg.alert("\n🕷️ Developer Hacks Sc Growtopia\t\t\t\tBy ZxTeamProjects\n\n⎚ Thx Theme From Cyrust\n\n🎮 Growtopia Hacks Script\n")
+end
+
+
+function exit()
+gg.clearResults()
+gg.setVisible(true)
+gg.toast('JetZ GT')
+print("")
+print("▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱")
+print("\n🕷️ Developer Hacks Sc Growtopia By ZxTeamProjects\n\n⎚ Thx Theme From Cyrust\n\n🎮 Growtopia Hacks Script\n")
+print("👥 Partnership :\nᶻˣJᴇᴛZ\nᶻˣXᴀᴋᴇX\n")
+print("💬 Join Telegram :\nhttps://t.me/ZXTEAMS\n")
+print("🃏Date and Time")
+print(os.date("📆 : %A , %d - %m - %Y "))
+print(os.date("⌚ : %H : %M : %S %p"))
+print("")
+print("▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱")
+gg.clearList()
+gg.clearResults()
+os.exit()
+end
+
+function srry()
+for i = 1,100000 do
+os.exit()
+end
+end
+
+
+while true do
+if gg.isVisible(true) then
+Jz = 1
+gg.setVisible(false)
+end
+if Jz == 1 then Main() end
+end
